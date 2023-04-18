df3c9a7248204d6aa4c0cc2af4a27c602faffcf8
Oscar Velandia <oscar.daniel.v.s.235@gmail.com>
2023-04-18 00:26:40 -0500

    Add validation to code input

--- src/components/CodeDialog.tsx
+++ src/components/CodeDialog.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import * as Form from "@radix-ui/react-form";
+import { ConfirmationFormContainer } from "./index";
 
 import styles from "./CodeDialog.module.scss";
-import { ConfirmationFormContainer } from ".";
 
 const texts = {
   title: "Ingresa tu codigo",
@@ -17,11 +17,25 @@ export const CodeDialog = () => {
           title={texts.title}
           submitButtonLabel={texts.sendCode}
         >
-          <input
-            className={styles.formInput}
-            type="tel"
-            placeholder={texts.inputPlaceholder}
-          />
+          <Form.Field name="code">
+            <Form.Control
+              placeholder={texts.inputPlaceholder}
+              maxLength={4}
+              minLength={4}
+              pattern="[0-9]"
+              required
+              type="tel"
+            />
+            <Form.Message match="valueMissing">
+              Por favor agrega el còdigo.
+            </Form.Message>
+            <Form.Message match="tooShort">
+              Por favor agrega 4 digitos.
+            </Form.Message>
+            <Form.Message match="patternMismatch">
+              El còdigo solamente incluye nùmeros.
+            </Form.Message>
+          </Form.Field>
         </ConfirmationFormContainer>
       </div>
     </div>
