200e3c8b3b5b0c5d42ee009e9aff48dfac483c13
Oscar Velandia <oscar.daniel.v.s.235@gmail.com>
2023-04-19 22:51:12 -0500

    Add progress in lottery

--- src/db/guests.ts
+++ src/db/guests.ts
@@ -1,4 +1,3 @@
-import { database } from "@db/config";
 import {
   CollectionReference,
   collection,
@@ -8,12 +7,15 @@ import {
   updateDoc,
   where,
 } from "firebase/firestore";
+import { getRandomItemIndex } from "../utils/getRandomItemIndex";
+import { database } from "./config";
 
 export interface Guest {
   isAdmin: boolean;
   cellphone?: string;
   code: string;
   genre: "male" | "female";
+  hasWon?: boolean;
   isBacheloretteFormHidden: boolean;
   menu?: "vegetarian" | "carnivorous";
   name: string;
@@ -22,6 +24,10 @@ export interface Guest {
 }
 
 const guestsReference = collection(database, "guests") as CollectionReference<Guest>;
+
+export const getAllGuests = async () => {
+  return (await getDocs(guestsReference)).docs
+}
 
 export const getGuestByCode = async (code: string) => {
   const guestQuery = query(guestsReference, where("code", "==", code));
@@ -36,4 +42,23 @@ const documentReferenceById = (id: string) => {
 
 export const updateGuest = async (guestId: string, data: Guest) => {
   await updateDoc(documentReferenceById(guestId), { ...data });
+};
+
+type FemaleGuests = Array<Guest & { id: string}>
+
+export const setLotteryWinner = async () => {
+	const guests = await getAllGuests();
+	const femaleGuests = guests.reduce((selectedGuests: FemaleGuests, currentGuest) => {
+		const guest = currentGuest.data();
+		
+		if (guest.genre === 'female') selectedGuests.push({...guest, id: currentGuest.id})
+		
+		return selectedGuests
+	}, [])
+  const winnerIndex = getRandomItemIndex(femaleGuests.length);
+  const {id: winnerId, ...winner} = femaleGuests[winnerIndex];
+
+	await updateDoc(documentReferenceById(winnerId), {...winner, hasWon: true});
+
+  return winner;
 };
--- src/pages/sorteo.tsx
+++ src/pages/sorteo.tsx
@@ -1,12 +1,9 @@
-import {
-  PagesContainer,
-  Separator,
-  SubtitleWithDate
-} from "@components/index";
+import { PagesContainer, Separator, SubtitleWithDate } from "@components/index";
 import { useRouter } from "next/router";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
+import * as _Progress from "@radix-ui/react-progress";
 import { GlobalContext } from "../context";
-import { Lottery } from "./api/lotteryWinner";
+import { setLotteryWinner } from "../db/guests";
 
 import styles from "./sorteo.module.scss";
 
@@ -18,16 +15,17 @@ const texts = {
   winner: "Ganadora",
 };
 
+const PROGRESS_COMPLETE = 100;
+const PROGRESS_MIN = 0;
+
 export default function Lottery() {
-  const { dispatch, state } = useContext(GlobalContext);
   const router = useRouter();
-
-  const handleRaffleButtonClick = async () => {
-    const response = await fetch("/api/lotteryWinner");
-    const lotteryWinner: Lottery = await response.json();
-
-    dispatch({ type: "SET_WINNER", payload: lotteryWinner });
-  };
+  const { dispatch, state } = useContext(GlobalContext);
+  const [isLoading, setIsLoading] = useState(false);
+  const [progress, setProgress] = useState(PROGRESS_MIN);
+  const [intervalId, setIntervalId] = useState<NodeJS.Timer | null>(null);
+  const isProgressVisible = progress < PROGRESS_COMPLETE && progress > PROGRESS_MIN;
+  const isProgressComplete = progress >= PROGRESS_COMPLETE && state.winner;
 
   useEffect(() => {
     if (!state.guest) return;
@@ -36,36 +34,71 @@ export default function Lottery() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [state.guest, state.guest?.isAdmin]);
 
+  useEffect(() => {
+    if (progress >= PROGRESS_COMPLETE) {
+      clearInterval(intervalId as NodeJS.Timer);
+      setIsLoading(false);
+    }
+  }, [intervalId, progress]);
+
+  const handleRaffleButtonClick = async () => {
+    setIsLoading(true);
+    setProgress((oldProgress) => 2 + oldProgress);
+
+    const winner = await setLotteryWinner();
+
+    dispatch({ type: "SET_WINNER", payload: winner });
+
+    const timerId = setInterval(
+      () => setProgress((oldProgress) => 5 + oldProgress),
+      500
+    );
+
+    setIntervalId(timerId);
+  };
+
   return (
     <PagesContainer gap="2rem">
       <section className={styles.container}>
         <SubtitleWithDate />
-        <h2 className={styles.title}>{state.winner ? texts.winner : texts.title}</h2>
-        {state.winner ? (
+        <h2 className={styles.title}>
+          {isProgressComplete ? texts.winner : texts.title}
+        </h2>
+        {isProgressVisible ? <Progress progress={progress} /> : null}
+        {isProgressComplete ? (
           <div className={styles.winnerContainer}>
             <div>
               <h3>{texts.name}</h3>
-              <h3>{state.winner.winner}</h3>
+              <h3>{state.winner?.name}</h3>
             </div>
             <div>
               <h3>{texts.code}</h3>
-              <h3>{state.winner.code}</h3>
+              <h3>{state.winner?.code}</h3>
             </div>
           </div>
         ) : (
           <button
+            aria-disabled={isLoading}
             className={styles.submitButton}
+            disabled={isLoading}
             onClick={handleRaffleButtonClick}
           >
             {texts.submitButton}
           </button>
-          // <SubmitAndCancelButtons
-          //   onSubmitButtonClick={handleRaffleButtonClick}
-          //   submitLabel={texts.submitButton}
-          // />
         )}
       </section>
       <Separator />
     </PagesContainer>
   );
 }
+
+const Progress = ({ progress }: { progress: number }) => {
+  return (
+    <_Progress.Root className={styles.progressRoot} value={progress}>
+      <_Progress.Indicator
+        className={styles.progressIndicator}
+        style={{ transform: `translateX(-${100 - progress}%)` }}
+      />
+    </_Progress.Root>
+  );
+};
